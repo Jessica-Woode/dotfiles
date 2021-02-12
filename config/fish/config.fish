46829a8d6ae567dc67b362338165bfe064975f6e
Jessica Woode <jess.woode.1995@gmail.com>
2021-02-12 13:52:29 -0500

    migrated configs to a central dir

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+## aliases
+alias kfish="kak ~/dotfiles/config/fish/config.fish"
+alias kbspwm="kak ~/dotfiles/config/bspwm/bspwmrc"
+alias ksxhkd="kak ~/dotfiles/config/sxhkd/sxhkdrc"
+alias kalacritty="kak ~/dotfiles/config/alacritty/alacritty.yml"
+alias kstarship="kak ~/.config/starship/starship.toml"
+alias p="sudo pacman"
+alias lg="lazygit"
+starship init fish | source
